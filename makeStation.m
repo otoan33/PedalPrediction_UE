d94db7f0b8b7bb11f5f615da5cae52142a5f4618
otoan33 <otoan33@users.noreply.github.com>
2021-01-25 15:49:32 +0900

    update main_01

--- makeStation.m
+++ makeStation.m
@@ -1,20 +1,46 @@
 clear
 close all
+% Precar 1-1
+% v1 = 40/3.6;
+% v2 = 30/3.6;
+% v3 = 45/3.6;
 
-v1 = 40/3.6;
-v2 = 30/3.6;
-v3 = 45/3.6;
+% s1 = 14.5;
+% s2 = 600;
+% s3 = 1000;
 
-s1 = 400;
-s2 = 600;
-s3 = 1000;
+% Precar 1-2
+% v1 = 20/3.6;
+% v2 = 45/3.6;
+% v3 = 60/3.6;
+
+% s1 = 14.5;
+% s2 = 314.5;
+% s3 = 1114.5;
+
+% % Precar 1-3
+v1 = 35/3.6;
+v2 = 40/3.6;
+v3 = 40/3.6;
+
+s1 = 247;
+s2 = 763;
+s3 = 800;
+
+% Precar 1-4
+% v1 = 20/3.6;
+% v2 = 30/3.6;
+% v3 = 45/3.6;
+
+% s1 = 13.5;
+% s2 = 313.5;
+% s3 = 800;
 
 Time = (0 : 0.025 : 100).';
 dT = 0.025;
 
 Station = array2table(Time);
 Station.S = zeros(height(Station),1);
-
 
 
 for i = 2 : height(Station)
@@ -34,3 +60,5 @@ for i = 2 : height(Station)
 
     Station.S(i) = Station.S(i-1) + dT*v;
 end
+
+writetable(Station, "./map_data/map_UE_1_Precar3.xlsx");
